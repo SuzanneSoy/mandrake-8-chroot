12ef03361f913ce40fd34667f6465574e31a7503
Georges Dupéron <georges.duperon@gmail.com>
2018-04-20 00:26:59 +0200

    Add mount of /proc

--- mount.sh
+++ mount.sh
@@ -5,3 +5,4 @@ sudo losetup -o $((512*6843753)) /dev/loop1 mandrake.raw
 sudo mount /dev/loop0 /mnt
 sudo mount /dev/loop1 /mnt/home
 sudo mount -t devpts devpts /mnt/dev/pts
+sudo mount -t proc proc /mnt/proc
